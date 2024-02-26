7e546045b3319d7f8c2496e90b4bbdd198cb206b
Diego Medina <demedinag17@gmail.com>
2024-02-26 00:09:37 -0300

    pequeños ajustes en index.html y index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     $('a').on('click',function(event){
-    if(this.hash !== ""){
-        event.preventdefault();
-        var hash = this.hash;
+        if(this.hash !== ""){
+            event.preventdefault();
+            var hash = this.hash;
 
-        $('heather,main').animate({
-            scrolltop: $(hash).offset().top                    
-        },800,function(){
-            window.location.hash = hash;
-        });      
-    }
-        });
+            $('header,main').animate({
+                scrolltop: $(hash).offset().top                    
+            },800,function(){
+                window.location.hash = hash;
+            });      
+        }
     });
-      
+    $('#tooltip').tooltip({title: "Visitanos en GitHub",trigger: "hover", placement: "top"});
+    $('#tooltip2').tooltip({title: "Visitanos en Linkedin",trigger: "hover", placement: "top"});
+    $('#tooltip3').tooltip({title: "Visitanos en Twitter",trigger: "hover", placement: "top"});
+    $('#tooltip4').tooltip({title: "Visitanos en Facebook",trigger: "hover", placement: "top"});
+});
